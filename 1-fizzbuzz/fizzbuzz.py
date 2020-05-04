3c9aa44022f261f495837368bfbc198a71af2553
ftnext <takuyafjp+develop@gmail.com>
2020-05-04 23:42:37 +0900

    [feat] implement fizzbuzz to pass tests in 1 and 2

---
+++ 1-fizzbuzz/fizzbuzz.py
@@ -0,0 +1,2 @@
+def create(number: int) -> int:
+    return number
---
+++ 1-fizzbuzz/tests/test_fizzbuzz.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+import fizzbuzz
+
+
+class CreateTestCase(TestCase):
+    def test_fizzbuzz_1(self):
+        actual = fizzbuzz.create(1)
+        self.assertEqual(actual, 1)
+
+    def test_fizzbuzz_2(self):
+        actual = fizzbuzz.create(2)
+        self.assertEqual(actual, 2)
