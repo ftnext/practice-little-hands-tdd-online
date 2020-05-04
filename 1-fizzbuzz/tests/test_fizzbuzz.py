965f3ee7e2f0a2cc48ccf468e1545b7ef5920659
ftnext <takuyafjp+develop@gmail.com>
2020-05-04 23:51:28 +0900

    [refactor] rewrite with subTest

--- 1-fizzbuzz/tests/test_fizzbuzz.py
+++ 1-fizzbuzz/tests/test_fizzbuzz.py
@@ -4,13 +4,15 @@ import fizzbuzz
 
 
 class CreateTestCase(TestCase):
-    def test_fizzbuzz_1(self):
-        actual = fizzbuzz.create(1)
-        self.assertEqual(actual, "1")
-
-    def test_fizzbuzz_2(self):
-        actual = fizzbuzz.create(2)
-        self.assertEqual(actual, "2")
+    def test_should_return_number_string(self):
+        test_patterns = [
+            (1, "1"),
+            (2, "2"),
+        ]
+        for number, expected in test_patterns:
+            with self.subTest(number=number, expected=expected):
+                actual = fizzbuzz.create(number)
+                self.assertEqual(actual, expected)
 
     def test_fizzbuzz_3(self):
         actual = fizzbuzz.create(3)
